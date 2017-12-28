2e2b1640586f7c4f6030d1497efb79b95f24ee6e
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2017-12-27 18:09:35 -0600

    added page structure and pseudocode

---
+++ app.js
@@ -0,0 +1,33 @@
+// pseudo code 
+
+//get document ready 
+
+// setting up array of animals 
+
+var aniamls = [];
+
+//once document has been loaded, do the following:
+
+//loop thru array items and make them into buttons 
+
+//I think we'd have to also add the val attribute as well 
+
+
+//user clicks on button, call API to return 10 gifs 
+
+//display the gif image 
+//display the gif rating 
+
+
+// for each gif 
+    // click to pause
+    //OOH double click to start it 
+
+
+//form when they submit an animal 
+
+//add that to the array of buttons 
+//make the button display on the screen 
+//similar to the above, when that button is clicked, call api to return 10 gifts
+
+//nice to have -when mouse is on a button, change it's color 
